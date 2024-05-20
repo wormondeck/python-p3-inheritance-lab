97b124b1c12dced210cf04149e476fd6d25d34e2
Ronney Arias <ronneyarias@gmail.com>
2024-05-20 17:51:38 -0500

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name, knowledge = None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge is not None else []
     
-    def learn(self):
-        pass
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
+        
+        
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,20 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge = None):
+        super().__init__(first_name, last_name)
+        if knowledge is None:
+            knowledge = [
+                "str is a data type in Python",
+                "programming is hard, but it's worth it",
+                "JavaScript async web request",
+                "Python function call definition",
+                "object-oriented teacher instance",
+                "programming computers hacking learning terminal",
+                "pipenv install pipenv shell",
+                "pytest -x flag to fail fast",
+            ]
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
